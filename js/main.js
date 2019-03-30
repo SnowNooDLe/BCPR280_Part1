ff6ee224b61254d0a772495007404f6cdbb4aa37
SnowNooDLe <sdw123123@gmail.com>
2019-03-30 15:22:13 +1300

    Keep Q2 seperate for validator

--- js/main.js
+++ js/main.js
@@ -11,11 +11,12 @@ class Game {
     }
     // method for Q1
     compareNumbersQ1(userInput, randomNum){
+        // Added after Test#4, checking empty string for guessing step
         if (userInput === ''){
             return "Invalid Try, please put value in"
         }
         if (userInput > 99 || userInput < 0){
-            return "Number is between 0 and 99"
+            return "Randomly generated number is between 0 and 99"
         }
         this.count++
         if (userInput < randomNum){
@@ -25,29 +26,6 @@ class Game {
         } else if (userInput == randomNum){
             return `You got it in ${this.count} trials!`
         }  else{
-            return "Something is not right !"
-        }
-    }
-
-    // method for Q2
-    compareNumbersQ2(userInput, randomNum){
-        let difference = Math.abs(userInput - randomNum)
-        if (userInput > 99 || userInput < 0){
-            return "Number is between 0 and 99"
-        }
-        this.count++
-        if (difference >= 40){
-            return "COLD"
-        } else if (difference >= 20 && difference <= 39){
-            return "COOL"
-        } else if (difference >= 10 && difference <= 19){
-            return "WARM"
-        } else if (difference >= 1 && difference <= 9){
-            return "HOT"
-        } else if (difference == 0){
-            return `You got it in ${this.count} trials!`
-        } 
-        else{
             return "Something is not right !"
         }
     }
@@ -81,27 +59,7 @@ var Q1 = new Vue({
     }
 });
 
-// CONTROLLER
-// working on Q2
-var Q2 = new Vue({
-    el:'#appendixTwoTwo',
-    data: {
-        game: new Game(),
-        randomNum: 0,
-        count: 0,
-        txtInput: '',
-        result: ''
-    },
-    methods: {
-        compareNumbers: function () {
-            this.randomNum = this.game.generateNumberByGame()
-            console.log("User input is " + this.txtInput)
-            console.log("Generated number is " + this.randomNum)
-            this.result = this.game.compareNumbersQ2(this.txtInput, this.randomNum)
-            this.count = this.game.getCountValue()
-        }
-    }
-});
+
 
 
 
