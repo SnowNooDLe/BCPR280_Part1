0b5413983b4bc42c75ea8e700b520d259092986a
SnowNooDLe <sdw123123@gmail.com>
2019-04-12 17:06:37 +1200

    fix test 2 and update naming convention

--- spec/question02Spec.js
+++ spec/question02Spec.js
@@ -1,83 +1,63 @@
-/* global describe  */
-describe('Game Two', () => {
-  // setup game area
-  describe('Create Game Two', () => {
-    let gameSetup
-    beforeEach(() =>{
-      gameSetup = new Game()
-    })
-
-    // checking whether its generating random number 
-    it('should return a random number', () => {
-      expect(gameSetup.generateNumberByGame()).toMatch(/\d{1,}/)
-    })
-
-    // Checking whether its initial count value is 0
-    it('Initial count value must be 0', () => {
-      expect(gameSetup.getCountValue()).toMatch(0)
+describe('Testing Game Two', () => {
+  let gameTwoTest
+  beforeEach(() => {
+    gameTwoTest = new Q2()
+    gameTwoTest.generatedNum = 30
+  })
+  // Test #1
+  describe('Test #1 "To determine if the program responses "COLD" ', () => {
+    var userInput = 80
+    it('Should print "COLD" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('COLD')
     })
   })
-  describe('Testing Game One', () => {
-    let gameTwoTest
-    beforeEach(() => {
-      gameTwoTest = new Q2()
-      gameTwoTest.generatedNum = 30
+  // Test #2
+  describe('Test #2 "To determine if the program responses "COOL" ', () => {
+    var userInput = 60
+    it('Should print "COOL" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('COOL')
     })
-    // Test #1
-    describe('Test #1 "To determine if the program responses "COLD" ', () => {
-      var userInput = 80
-      it('Should print "COLD" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('COLD')
-      })
+  })
+  // Test #3
+  describe('Test #3 "To determine if the program responses "WARM" ', () => {
+    var userInput = 45
+    it('Should print "WARM" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('WARM')
     })
-    // Test #2
-    describe('Test #2 "To determine if the program responses "COOL" ', () => {
-      var userInput = 60
-      it('Should print "COOL" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('COOL')
-      })
+  })
+  // Test #4
+  describe('Test #4 "To determine if the program responses "HOT" ', () => {
+    var userInput = 31
+    it('Should print "HOT" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('HOT')
     })
-    // Test #3
-    describe('Test #3 "To determine if the program responses "WARM" ', () => {
-      var userInput = 45
-      it('Should print "WARM" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('WARM')
-      })
+  })
+  // Test #5
+  describe('Test #5 "To determine if the program responses "You got it in n trials!" ', () => {
+    var userInput = 30
+    it('Should print "You got it in 1 trials!" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('You got it in 1 trials!')
     })
-    // Test #4
-    describe('Test #4 "To determine if the program responses "HOT" ', () => {
-      var userInput = 31
-      it('Should print "HOT" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('HOT')
-      })
+  })
+  // Test #6
+  describe('Test #6 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
+    var userInput = ''
+    it('Should print "Invalid Try, please put integer value in to guess" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
     })
-    // Test #5
-    describe('Test #5 "To determine if the program responses "You got it in n trials!" ', () => {
-      var userInput = 30
-      it('Should print "You got it in 1 trials!" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('You got it in 1 trials!')
-      })
+  })
+  // Test #7
+  describe('Test #7 "To determin if the program tells user that randomly generated number is between 0 and 99 ', () => {
+    var userInput = 105
+    it('Should print "Randomly generated number is between 0 and 99" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('Randomly generated number is between 0 and 99')
     })
-    // Test #6
-    describe('Test #6 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
-      var userInput = ''
-      it('Should print "Invalid Try, please put integer value in to guess" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
-      })
-    })
-    // Test #7
-    describe('Test #7 "To determin if the program tells user that randomly generated number is between 0 and 99 ', () => {
-      var userInput = 105
-      it('Should print "Randomly generated number is between 0 and 99" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('Randomly generated number is between 0 and 99')
-      })
-    })
-    // Test #8
-    describe('Test #8 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
-      var userInput = 'abcd'
-      it('Should print "Invalid Try, please put integer value in to guess" ', () => {
-        expect(gameTwoTest.compareNumbersQ1(userInput, gameTwoTest.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
-      })
+  })
+  // Test #8
+  describe('Test #8 "To determine if the program responses "Invalid Try, please put integer value" ', () => {
+    var userInput = 'abcd'
+    it('Should print "Invalid Try, please put integer value in to guess" ', () => {
+      expect(gameTwoTest.compareNumbersQ2(userInput, gameTwoTest.generatedNum)).toBe('Invalid Try, please put integer value in to guess')
     })
   })
 })
--- src/main.js
+++ src/main.js
@@ -46,6 +46,38 @@ class Q1 extends Game {
         }
     }
 }
+
+// Class for Q2
+class Q2 extends Game {
+    compareNumbersQ2(input, randomNum){
+        // checking input is valid integer or not reuse from Q1
+        if (!Number.isInteger(input)){
+            return "Invalid Try, please put integer value in to guess"
+        }
+
+        if (input > 99 || input < 0){
+            return "Randomly generated number is between 0 and 99"
+        }
+        // as we are talking the range from target number, absolute number is perfect to use
+        let difference = Math.abs(input - randomNum)
+        
+        this.count++
+        if (difference >= 40){
+            return "COLD"
+        } else if (difference >= 20 && difference <= 39){
+            return "COOL"
+        } else if (difference >= 10 && difference <= 19){
+            return "WARM"
+        } else if (difference >= 1 && difference <= 9){
+            return "HOT"
+        } else if (difference == 0){
+            return `You got it in ${this.count} trials!`
+        } 
+        else{
+            return "Something is not right !"
+        }
+    }
+}
 // Controller for Q1
 var question1 = new Vue({
     el:'#appendixTwoOne', 
@@ -89,37 +121,6 @@ var question1 = new Vue({
     }
 });
 
-// Class for Q2
-class Q2 extends Game {
-    compareNumbersQ2(input, randomNum){
-        // checking input is valid integer or not reuse from Q1
-        if (!Number.isInteger(input)){
-            return "Invalid Try, please put integer value in to guess"
-        }
-
-        if (input > 99 || input < 0){
-            return "Number is between 0 and 99"
-        }
-        // as we are talking the range from target number, absolute number is perfect to use
-        let difference = Math.abs(input - randomNum)
-        
-        this.count++
-        if (difference >= 40){
-            return "COLD"
-        } else if (difference >= 20 && difference <= 39){
-            return "COOL"
-        } else if (difference >= 10 && difference <= 19){
-            return "WARM"
-        } else if (difference >= 1 && difference <= 9){
-            return "HOT"
-        } else if (difference == 0){
-            return `You got it in ${this.count} trials!`
-        } 
-        else{
-            return "Something is not right !"
-        }
-    }
-}
 // Controller for Q2
 var question2 = new Vue({
     el:'#appendixTwoTwo',
